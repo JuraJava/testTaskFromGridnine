0bb7079111f0890be1a350a2e5b9112fe04e2d5e
YurDan1973 <jura.danilov.73@gmail.com>
2026-04-30 09:38:34 +0300

    Добавление

--- src/test/java/com/gridnine/testing/MainTest.java
+++ src/test/java/com/gridnine/testing/MainTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-// С JUnit. В задании было указано чтобы никкаких библиотек не добавлять. (У меня в IntelliJ IDEA была добавлена ранее)
+// С JUnit. В задании было указано чтобы никкаких библиотек не добавлять. (У меня в IntelliJ IDEA была добавлена ранее).
 public class MainTest {
 
     @Test
